520ef206e6600a6e6c252180945f521ab42d6281
Andrej Hristovski <andrejhristovski@outlook.com>
2023-01-20 10:06:43 +0100

    Add new search bar component from mui library

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,17 +1,31 @@
-import SearchBar from "material-ui-search-bar";
-import React from "react";
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
-const Search = () => {
+export default function SearchBar() {
   return (
-    <div style={{ width: "400px", marginLeft: "180px" }}>
-      <SearchBar
-        style={{ borderRadius: "32px", height: "30px" }}
-        value={""}
-        placeholder={"Search Planet Cassandra..."}
-        className="fontSize"
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 400,
+        height: 30,
+        borderRadius: 50,
+        marginLeft: 10,
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Planet Cassandra..."
+        inputProps={{ "aria-label": "search google maps" }}
       />
-    </div>
+      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
-};
-
-export default Search;
+}
