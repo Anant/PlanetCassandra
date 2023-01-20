949b3d65cd0c110adbb3a486d69a1d61c0fe5ea8
Andrej Hristovski <andrejhristovski@outlook.com>
2023-01-20 11:14:11 +0100

    Refactor hero section and add styles to text components

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,66 +1,136 @@
-import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby'
-import { Grid, Typography, Button, Container } from '@mui/material'
-import Carousel from './Carousel/Carousel';
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import { Grid, Typography, Button, Container, Box } from "@mui/material";
+import Carousel from "./Carousel/Carousel";
+import SendIcon from "@mui/icons-material/Send";
 
 interface AllWpPostData {
-    allWpPost: {
-        nodes: {
-            title: string;
-            slug: string;
-            featuredImage: {
-                node: {
-                    publicUrl: string;
-                    localFile: {
-                        relativePath: string;
-                        childImageSharp: {
-                            fluid: {
-                                src: string;
-                            }
-                        }
-                    }
-                }
-            }
-        }[]
-    }
+  allWpPost: {
+    nodes: {
+      title: string;
+      slug: string;
+      featuredImage: {
+        node: {
+          publicUrl: string;
+          localFile: {
+            relativePath: string;
+            childImageSharp: {
+              fluid: {
+                src: string;
+              };
+            };
+          };
+        };
+      };
+    }[];
+  };
 }
-
 
 const HeroSection = () => {
+  const { allWpPost } = useStaticQuery(query);
 
-    const { allWpPost } = useStaticQuery(query);
+  //Mapping the results from the query and skiping the ones that don't have an image
+  const items = allWpPost.nodes.map(
+    ({
+      title,
+      featuredImage,
+      slug,
+    }: AllWpPostData["allWpPost"]["nodes"][0]) => ({
+      title,
+      image: featuredImage?.node?.localFile?.childImageSharp?.fluid?.src,
+      slug,
+    })
+  );
 
-    //Mapping the results from the query and skiping the ones that don't have an image
-    const items = allWpPost.nodes.map(({ title, featuredImage, slug }: AllWpPostData['allWpPost']['nodes'][0]) => ({
-        title,
-        image: featuredImage?.node?.localFile?.childImageSharp?.fluid?.src,
-        slug
-    }));
-
-    return (
-        <Container>
-        <Grid container maxWidth="lg">
-            <Grid item xs={12} md={6}>
-                <Typography variant="h3" component="h1">
-                    Welcome to Planet Cassandra
-                </Typography>
-                <Typography variant="subtitle1">
-                    The best knowledge base on Apache Cassandra to help platform leaders, architects, engineers, and operators to build scalable platforms
-                </Typography>
-                <Button variant="contained" color="primary">
-                    Get Started
-                </Button>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Carousel items={items} />
-            </Grid>
+  return (
+    <Container>
+      <Grid
+        sx={{
+          height: 700,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        container
+        maxWidth="lg"
+      >
+        <Grid item xs={12} md={6}>
+          <Typography variant="h3" component="h1">
+            Welcome to
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+            }}
+          >
+            <Typography
+              fontWeight={"bold"}
+              color="#344D67"
+              component="h1"
+              variant="h3"
+              gutterBottom
+            >
+              Planet.
+            </Typography>
+            <Typography
+              fontWeight={"bold"}
+              component="h1"
+              variant="h3"
+              color="#5AB1BB"
+              align={"center"}
+              gutterBottom
+            >
+              Cassandra
+            </Typography>
+          </Box>
+          <Typography
+            sx={{
+              marginTop: 5,
+            }}
+            variant="subtitle1"
+          >
+            The best knowledge base on Apache Cassandra to help platform
+            leaders, architects, engineers, and operators to build scalable
+            platforms
+          </Typography>
+          <Box
+            sx={{
+              marginTop: 5,
+            }}
+          >
+            <Button
+              sx={{
+                borderRadius: 50,
+                backgroundColor: "#5AB1BB",
+              }}
+              variant="contained"
+              endIcon={<SendIcon />}
+            >
+              Get Started
+            </Button>
+            <Button
+              sx={{
+                borderRadius: 50,
+                marginLeft: 10,
+                backgroundColor: "#344D67",
+              }}
+              variant="contained"
+              endIcon={<SendIcon />}
+            >
+              Meet with us
+            </Button>
+          </Box>
         </Grid>
-        </Container>
-    )
-}
+        <Grid item xs={12} md={6}>
+          <Carousel items={items} />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export const query = graphql`
-query GET_POSTS {
+  query GET_POSTS {
     allWpPost {
       nodes {
         title
