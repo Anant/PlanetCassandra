0a8e33411036b8aefa0a16d122e2ab0af9696de9
Andrej Hristovski <andrejhristovski@outlook.com>
2023-01-23 15:24:36 +0100

    src/images/PostTag.jpg
    Add placeholder image for tag posts

---
+++ src/components/TagSection/TagCard.tsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { StaticImage } from "gatsby-plugin-image";
+
+interface Props {
+  post: {
+    title: string;
+  };
+}
+const TagCard: React.FC<Props> = ({ post }) => {
+  return (
+    <Card sx={{ maxWidth: 345, borderRadius: 5, padding: 3 }}>
+      <Typography
+        sx={{
+          color: "#5AB1BB",
+        }}
+        gutterBottom
+        variant="h6"
+        component="div"
+      >
+        Featured article :
+      </Typography>
+      <StaticImage
+        style={{ borderRadius: 5 }}
+        src="../../images/PostTag.jpg"
+        alt="A dinosaur"
+        placeholder="blurred"
+      />
+      <CardContent sx={{ paddingInline: 0 }}>
+        <Typography
+          sx={{ height: 100 }}
+          gutterBottom
+          variant="h6"
+          component="div"
+        >
+          {post.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          cum ducimus expedita recusandae esse nihil, aliquid quo quia atque
+          quibusdam amet inventore pariatur, consectetur earum dolore a
+          exercitationem laborum tempora.
+        </Typography>
+      </CardContent>
+      <CardActions
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingInline: 0,
+        }}
+      >
+        <Button
+          sx={{
+            borderRadius: 50,
+            backgroundColor: "#5AB1BB",
+            fontSize: 10,
+            "&:hover": {
+              backgroundColor: "#5AB1BB",
+            },
+          }}
+          variant="contained"
+        >
+          Continue reading
+        </Button>{" "}
+        <Button
+          sx={{
+            borderRadius: 50,
+            backgroundColor: "#F2545B",
+            fontSize: 10,
+            "&:hover": {
+              backgroundColor: "#F2545B",
+            },
+          }}
+          variant="contained"
+        >
+          See all articles
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default TagCard;
--- src/components/TagSection/TagSection.tsx
+++ src/components/TagSection/TagSection.tsx
@@ -1,66 +1,89 @@
-import React, { useState } from 'react';
-import { Grid, Typography, Container, Button, Card, CardContent } from '@mui/material'
-import { useStaticQuery, graphql } from "gatsby"
-import TagPosts from './TagPosts';
-import './tagStyles.css';
+import React, { useState } from "react";
+import {
+  Grid,
+  Typography,
+  Container,
+  Button,
+  Card,
+  CardContent,
+} from "@mui/material";
+import { useStaticQuery, graphql } from "gatsby";
+import TagPosts from "./TagPosts";
+import "./tagStyles.css";
 
 interface Data {
-    id: string;
-    title: string;
-    tag: string;
+  id: string;
+  title: string;
+  tag: string;
 }
 
 interface AllWpTagData {
-    data: Data[];
-    allWpTag: {
+  data: Data[];
+  allWpTag: {
+    nodes: {
+      name: string;
+      id: string;
+      posts: {
         nodes: {
-            name: string;
-            id: string;
-            posts: {
-                nodes: {
-                    title: string;
-                }[]
-            }
-        }[]
-    }
+          title: string;
+        }[];
+      };
+    }[];
+  };
 }
 
 const TagSection = () => {
-    const { allWpTag }: AllWpTagData = useStaticQuery(query);
-    const tags = allWpTag.nodes
-    
+  const { allWpTag }: AllWpTagData = useStaticQuery(query);
+  const tags = allWpTag.nodes;
 
-    const [selectedTag, setSelectedTag] = useState<string | null>(tags[0].name);
-    const selectedTagData = allWpTag.nodes.filter(node => node.name === selectedTag);
-    const posts = selectedTagData.length > 0 ? selectedTagData[0].posts.nodes : [];
+  const [selectedTag, setSelectedTag] = useState<string | null>(tags[0].name);
+  const selectedTagData = allWpTag.nodes.filter(
+    (node) => node.name === selectedTag
+  );
+  const posts =
+    selectedTagData.length > 0 ? selectedTagData[0].posts.nodes : [];
 
-    const handleClick = (tag: { name: string; id: string }) => {
-        setSelectedTag(tag.name);
-    };
-   
-    return (
-        <Container>
-            <Grid>
-                {tags.map(tag => (
-                    <Button
-                        key={tag.id}
-                        onClick={() => handleClick(tag)}
-                        className={selectedTag === tag.name ? `selected` : ''}
-                    >
-                        {tag.name}
-                    </Button>
-                ))}
-            </Grid>
-            <Grid>
-            {selectedTag ? <TagPosts tag={selectedTag} posts={posts} /> : null}
-            </Grid>
-        </Container>
-    );
+  const handleClick = (tag: { name: string; id: string }) => {
+    setSelectedTag(tag.name);
+  };
+
+  return (
+    <Container>
+      <Grid sx={{ marginY: 3 }} rowSpacing={3} columnSpacing={1} container>
+        {tags.map((tag) => (
+          <Grid key={tag.id} item md={2} xs={4}>
+            <Button
+              sx={{
+                backgroundColor: "#344D67",
+                color: "white",
+                borderRadius: 50,
+                padding: 1,
+                minWidth: 150,
+                textAlign: "center",
+                ":hover": {
+                  backgroundColor: "#5ab1bb",
+                  color: "white",
+                },
+              }}
+              variant="contained"
+              onClick={() => handleClick(tag)}
+              className={selectedTag === tag.name ? `selected` : ""}
+            >
+              {tag.name}
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid>
+        {selectedTag ? <TagPosts tag={selectedTag} posts={posts} /> : null}
+      </Grid>
+    </Container>
+  );
 };
 
 export const query = graphql`
-query GET_TAGS {
-    allWpTag(sort: {count: DESC}, filter: {count: {ne: null}}, limit: 5) {
+  query GET_TAGS {
+    allWpTag(sort: { count: DESC }, filter: { count: { ne: null } }, limit: 5) {
       nodes {
         name
         id
