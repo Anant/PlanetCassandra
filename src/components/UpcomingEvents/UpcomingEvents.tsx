c0a686d65cdb60194761f46a2793c63b7469f4ce
Andrej Hristovski <andrejhristovski@outlook.com>
2023-01-24 15:05:47 +0100

    Add upcoming events section component

---
+++ src/components/UpcomingEvents/UpcomingEvents.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Grid, Typography, Button, Container, Box } from "@mui/material";
+import EventCard from "../EventsCards/EventCard";
+import AllEventsCard from "../EventsCards/AllEventsCard";
+import "./index.css";
+const UpcomingEvents = () => {
+  return (
+    <Grid className="bgImage" container>
+      <Container
+        sx={{
+          paddingY: 10,
+        }}
+      >
+        <Typography
+          sx={{ marginBottom: 3, textAlign: { xs: "center", md: "start" } }}
+          color="white"
+          variant="h4"
+        >
+          Upcoming Events :
+        </Typography>
+        <Grid rowSpacing={3} container>
+          <Grid item xs={12} md={8}>
+            <EventCard />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <AllEventsCard />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grid>
+  );
+};
+
+export default UpcomingEvents;
