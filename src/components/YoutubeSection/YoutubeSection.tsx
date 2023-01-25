880cb69293eeb2b8dca61f42aeb67f33eca4aa7f
Andrej Hristovski <andrejhristovski@outlook.com>
2023-01-25 16:01:01 +0100

    Add youtube section

---
+++ src/components/YoutubeSection/YoutubeSection.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Grid, Typography, Container } from "@mui/material";
+
+const YoutubeSection = () => {
+  return (
+    <Container>
+      <Grid
+        marginY={10}
+        justifyContent="center"
+        alignItems="center"
+        container
+        maxWidth="lg"
+      >
+        <Grid item xs={12} md={6}>
+          <Typography variant="h3" component="h1">
+            Want to become a
+          </Typography>
+          <Grid container>
+            <Typography
+              fontWeight={"bold"}
+              color="#344D67"
+              component="h1"
+              variant="h3"
+              gutterBottom
+            >
+              pro in
+            </Typography>
+            <Typography
+              marginLeft={2}
+              fontWeight={"bold"}
+              component="h1"
+              variant="h3"
+              color="#5AB1BB"
+              align={"center"}
+              gutterBottom
+            >
+              Cassandra?
+            </Typography>
+          </Grid>
+          <Typography
+            sx={{
+              marginTop: 5,
+            }}
+            variant="subtitle1"
+          >
+            Visit our YouTube channel and look through our extensive library of
+            tutorials, quick fixes, pro tips, tricks and lunch ideas from some
+            of the biggest personalities in the industry.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <iframe
+            id="player"
+            width="600"
+            height="300"
+            src="https://www.youtube.com/embed/yQCDZDUzRJM"
+          ></iframe>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default YoutubeSection;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import { Grid } from "@mui/material";
+
 import HeroSection from "../components/Hero/Hero";
 import DidYouKnowSection from "../components/DYK Section/DYK";
 import Layout from "../components/Layout/Layout";
 import TagSection from "../components/TagSection/TagSection";
 import UpcomingEvents from "../components/UpcomingEvents/UpcomingEvents";
-import { useStaticQuery, graphql } from "gatsby";
-import { Grid } from "@mui/material";
+import YoutubeSection from "../components/YoutubeSection/YoutubeSection";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
@@ -23,6 +24,9 @@ const IndexPage: React.FC<PageProps> = () => {
         </Grid>
         <Grid item xs={12}>
           <UpcomingEvents />
+        </Grid>
+        <Grid item xs={12}>
+          <YoutubeSection />
         </Grid>
       </Grid>
     </Layout>
