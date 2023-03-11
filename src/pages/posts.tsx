87e71c1464c4f3c4bcf48e4e6c4add5c1237a2e8
StefanVinica <stefannikolovski996@gmail.com>
2023-03-11 03:38:12 -0500

    test

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,96 +1,96 @@
-// src/pages/posts.tsx
-import React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout/Layout';
-import PostCardGrid from '../layouts/PostCardGrid'
-import { IGatsbyImageData } from 'gatsby-plugin-image';
+// // src/pages/posts.tsx
+// import React from 'react'
+// import { graphql } from 'gatsby'
+// import Layout from '../components/Layout/Layout';
+// import PostCardGrid from '../layouts/PostCardGrid'
+// import { IGatsbyImageData } from 'gatsby-plugin-image';
 
-interface Props {
-  data: {
-    allWpPost: {
-      totalCount: number;
-      nodes: {
-        categories: {
-          nodes: {
-            name: string;
-            slug: string;
-            count: number;
-          }[];
-        };
-        author: {
-          node: {
-            avatar: {
-              url: string;
-            };
-            name: string;
-          };
-        };
-        date: string;
-        slug: string;
-        title: string;
-        featuredImage: {
-          node: {
-            localFile: {
-              childImageSharp: {
-                gatsbyImageData: IGatsbyImageData;
-              };
-            };
-          };
-        };
-        excerpt: string;
-      }[];
-    };
-  };
-}
+// interface Props {
+//   data: {
+//     allWpPost: {
+//       totalCount: number;
+//       nodes: {
+//         categories: {
+//           nodes: {
+//             name: string;
+//             slug: string;
+//             count: number;
+//           }[];
+//         };
+//         author: {
+//           node: {
+//             avatar: {
+//               url: string;
+//             };
+//             name: string;
+//           };
+//         };
+//         date: string;
+//         slug: string;
+//         title: string;
+//         featuredImage: {
+//           node: {
+//             localFile: {
+//               childImageSharp: {
+//                 gatsbyImageData: IGatsbyImageData;
+//               };
+//             };
+//           };
+//         };
+//         excerpt: string;
+//       }[];
+//     };
+//   };
+// }
 
-const Posts: React.FC<Props> = (props: Props) => {
-  const { data } = props;
-  const posts = data.allWpPost.nodes as any
+// const Posts: React.FC<Props> = (props: Props) => {
+//   const { data } = props;
+//   const posts = data.allWpPost.nodes as any
 
-  return (
-    <Layout>
-      <PostCardGrid cardData={posts} />
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       <PostCardGrid cardData={posts} />
+//     </Layout>
+//   );
+// };
 
 
-export const query = graphql`
-  query Posts {
-    allWpPost(sort: {date: DESC}) {
-      totalCount
-      nodes {
-        categories {
-          nodes {
-            name
-            slug
-            count
-          }
-        }
-        author {
-          node {
-            avatar {
-              url
-            }
-            name
-          }
-        }
-        date
-        slug
-        title
-        featuredImage {
-          node {
-            localFile {
-              childImageSharp {
-                gatsbyImageData
-              }
-            }
-          }
-        }
-        excerpt
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   query Posts {
+//     allWpPost(sort: {date: DESC}) {
+//       totalCount
+//       nodes {
+//         categories {
+//           nodes {
+//             name
+//             slug
+//             count
+//           }
+//         }
+//         author {
+//           node {
+//             avatar {
+//               url
+//             }
+//             name
+//           }
+//         }
+//         date
+//         slug
+//         title
+//         featuredImage {
+//           node {
+//             localFile {
+//               childImageSharp {
+//                 gatsbyImageData
+//               }
+//             }
+//           }
+//         }
+//         excerpt
+//       }
+//     }
+//   }
+// `;
 
-export default Posts;
+// export default Posts;
