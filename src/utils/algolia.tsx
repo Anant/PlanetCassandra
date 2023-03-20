d4a5e0678f57d91a97afa613fcb5b329d73144ab
StefanVinica <stefannikolovski996@gmail.com>
2023-03-20 18:46:41 +0100

    Algolia Indexing

--- src/utils/algolia.tsx
+++ src/utils/algolia.tsx
@@ -29,25 +29,58 @@ interface NewsAlgoliaData {
   };
 }
 
+interface LeavesAlgoliaData {
+  allApiLeaves: {
+    totalCount: number;
+    nodes: {
+      id: string;
+      title: string;
+      description: string;
+      tags: string[];
+    }[];
+  };
+}
 
-// const PostQuery = `
-// query PostsAlgolia {
-//     allWpPost(sort: {date: DESC}) {
-//       totalCount
-//       nodes {
-//         id
-//         author {
-//           node {
-//             name
-//           }
-//         }
-//         date
-//         slug
-//         title
-//       }
-//     }
-//   }
-// `;
+
+
+
+
+const PostQuery = `
+query PostsAlgolia {
+    allWpPost(sort: {date: DESC}) {
+      totalCount
+      nodes {
+        id
+        author {
+          node {
+            name
+          }
+        }
+        date
+        slug
+        title
+      }
+    }
+  }
+`;
+
+const LeavesAlgoliaQuery = `
+query LeavesAlgolia {
+  allApiLeaves(
+    sort: {wallabag_created_at: DESC}
+    limit: 250
+    filter: {title: {ne: null}}
+  ) {
+    nodes {
+      id
+      title
+      description
+      tags
+    }
+  }
+}
+`;
+
 
 const NewsQuery =`
 query NewsAlgolia {
@@ -62,14 +95,14 @@ query NewsAlgolia {
 `
 
 const queries = [
-    // {
-    //     query: PostQuery,
-    //     queryVariables: {},
-    //     transformer: ({ data }: { data: QueryResult }) => data.allWpPost.nodes,
-    //     indexName: 'PlanetCassandra',
-    //     settings: {},
-    //     mergeSettings: false,
-    // },
+    {
+        query: PostQuery,
+        queryVariables: {},
+        transformer: ({ data }: { data: QueryResult }) => data.allWpPost.nodes,
+        indexName: 'PlanetCassandraPosts',
+        settings: {},
+        mergeSettings: false,
+    },
     {
       query: NewsQuery,
       queryVariables: {},
@@ -77,7 +110,15 @@ const queries = [
       indexName: 'PlanetCassandraNews',
       settings: {},
       mergeSettings: false,
-  },
+    },
+    {
+      query: LeavesAlgoliaQuery,
+      queryVariables: {},
+      transformer: ({ data }: { data: LeavesAlgoliaData }) => data.allApiLeaves.nodes,
+      indexName: 'PlanetCassandraLeaves',
+      settings: {},
+      mergeSettings: false,
+    },
 ];
 
 module.exports = queries
