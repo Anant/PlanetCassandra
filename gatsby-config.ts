c267f4dd1f428508933b5f6a80b873aedfc57e1d
StefanVinica <stefannikolovski996@gmail.com>
2023-03-20 19:05:50 +0100

    diff envs for prod and dev

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -73,7 +73,7 @@ const config: GatsbyConfig = {
       resolve: `gatsby-source-wordpress`,
       options: {
         //Create auth and add it to ENV file
-        url: process.env.WP_GRAPHQL,
+        url:process.env.NODE_ENV === "development" ?process.env.WP_GRAPHQL : process.env.WP_GRAPHQL_DEV,
         type: {
           Post: process.env.NODE_ENV === "development" ? { limit: 20 } : { limit: 500 },
           Page: {
