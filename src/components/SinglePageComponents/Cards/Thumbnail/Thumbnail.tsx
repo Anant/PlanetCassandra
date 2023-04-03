c4b5f1e7c651b75fe03a868bdcae6f8d722e1a33
Andrej Hristovski <andrejhristovski@yahoo.com>
2023-04-03 17:30:13 +0200

    Add thumbnail component

---
+++ src/components/SinglePageComponents/Cards/Thumbnail/Thumbnail.tsx
@@ -0,0 +1,27 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { IGatsbyImageData } from "gatsby-plugin-image";
+interface ThumbnailInterface {
+  thumbnail: IGatsbyImageData;
+}
+const ThumbnailImage: React.FC<ThumbnailInterface> = ({
+  thumbnail,
+}: ThumbnailInterface) => {
+  console.log(thumbnail);
+
+  return (
+    <Box
+      sx={{
+        marginTop: 1,
+        width: "100%",
+        // height: imageHeight ? imageHeight : { sm: "250px", md: "600px" },
+      }}
+    >
+      <GatsbyImage alt="test" image={thumbnail} />
+      {/* Static image is not working so we must use img tag */}
+    </Box>
+  );
+};
+
+export default ThumbnailImage;
