87aa2b4d76362a0438c07c63a93e2dd6fe7afccb
StefanVinica <stefannikolovski996@gmail.com>
2023-04-10 14:35:38 +0200

    Promise Fix

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -32,6 +32,10 @@ interface AllPostsResult {
   data?: AllPostsData;
   errors?: any;
 }
+//@ts-ignore
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 //Needs work i need to alter the schema so airtable gets the localfile and the nodes we create from the images
 //@ts-ignore
@@ -59,16 +63,139 @@ export const createPages: GatsbyNode["createPages"] = async ({
 }) => {
   const { createPage, createNode, deletePage, createRedirect } = actions;
 
+  //await delay(202870);
   createRedirects(createRedirect);
   //@ts-ignore
   createPosts({ createPage, graphql });
   //@ts-ignore
   createEvents({ actions, graphql, createNodeId, getCache });
 
+
+
   // await createUseCases({ createPage, graphql, resolve });
   // await createTtrss({ createPage, graphql, resolve });
   // await createLeaves({ createPage, graphql, fetchThumbnail, createNode, createNodeId, getCache, resolve });
   // await createVideos({ createPage, graphql, resolve });
+
+
+  //----------------------------------------------------------------------------
+  //Leaves Pictures Processing
+  const allLeavesPictures: {
+    errors?: any;
+    data?: {
+      allApiLeaves: {
+        nodes: {
+          id: string;
+          preview_picture: string | null;
+          url: string;
+          origin_url: string;
+        }[];
+      };
+    };
+  } = await graphql(`
+    query LeavesPictures {
+      allApiLeaves(
+        filter: { url: { ne: null } }
+        limit: 200
+        sort: { wallabag_created_at: DESC }
+      ) {
+        nodes {
+          id
+          preview_picture
+          url
+          origin_url
+          wallabag_created_at
+        }
+      }
+    }
+  `);
+  const failingUrls = [
+    'blogs.vmware.com',
+    'https://www.how2shout.com/linux/2-ways-to-install-cassandra-on-ubuntu-22-04-lts-jammy/',
+    'blog.softwaremill',
+    'www.ktexperts',
+    'rustyrazorblade.com',
+    'https://docs.d2iq.com/mesosphere/dcos/services/cassandra/2.9.0-3.11.6/security/',
+    '/www.an10.io/',
+    'itnext.io',
+    '/www.an10.io/',
+    'baeldung.com',
+    '/levelup.gitconnected',
+    'cassandra.apache.org/blog',
+    'Failed to fetch thumbnail for event: https://docs.d2iq.com/mesosphere/dcos/services/cassandra/2.9.0-3.11.6/security/',
+    'datanami.com',
+    'https://dataedo.com/',
+    'https://www.datastax.com/resources/webinar/serverless-functions-datastax-drivers',
+    'https://towardsdatascience.com/',
+    'https://medium.com/better-programming/our-favorite-engineering-blogs-3d8365b2d871',
+    'https://datastation.multiprocess',
+    'tobert.github.io',
+    'zeppelin.apache.org',
+    'https://docs.datastax.com/en/articles/cassandra/cassandrathenandnow.html',
+  ];
+
+  const filteredNodes = allLeavesPictures?.data?.allApiLeaves?.nodes.filter(
+    (node) => {
+      return !failingUrls.some((url) => node.url.includes(url));
+    }
+  );
+
+  //@ts-ignore
+  const thumbnailPromises = filteredNodes.map((node, index) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        fetchThumbnail(node, createNode, createNodeId, getCache).then(() => {
+          resolve(true);
+        });
+      }, index * 200);
+    });
+  });
+
+  await Promise.all(thumbnailPromises);
+
+  //----------------------------------------------------------------------------
+  //TTRS Pictures Processing
+  const AllNews: {
+    errors?: any;
+    data?: {
+      allFeedTtrs: {
+        nodes: {
+          id: string;
+          link: string;
+        }[];
+      };
+    };
+  } = await graphql(`
+    query NewsPictures {
+      allFeedTtrs {
+        nodes {
+          link
+          id
+        }
+      }
+    }
+  `);
+  const filterednewsNodes = AllNews?.data?.allFeedTtrs?.nodes.filter((node) => {
+    return !failingUrls.some((url) => node.link.includes(url));
+  });
+
+  //@ts-ignore
+  const newsThumbnailPromises = filterednewsNodes.map((node, index) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const newNode = {
+          id: node.id,
+          url: node.link,
+          origin_url: node.link,
+        };
+        fetchThumbnail(newNode, createNode, createNodeId, getCache).then(() => {
+          resolve(true);
+        });
+      }, index * 200);
+    });
+  });
+
+  await Promise.all(newsThumbnailPromises);
 
   //UseCases
   ////----------------------------------------------------------------------------
@@ -178,113 +305,6 @@ export const createPages: GatsbyNode["createPages"] = async ({
         author: node.author,
       },
     });
-  });
-
-  //----------------------------------------------------------------------------
-  //Leaves Pictures Processing
-  const allLeavesPictures: {
-    errors?: any;
-    data?: {
-      allApiLeaves: {
-        nodes: {
-          id: string;
-          preview_picture: string | null;
-          url: string;
-          origin_url: string;
-        }[];
-      };
-    };
-  } = await graphql(`
-    query LeavesPictures {
-      allApiLeaves(
-        filter: { url: { ne: null } }
-        limit: 200
-        sort: { wallabag_created_at: DESC }
-      ) {
-        nodes {
-          id
-          preview_picture
-          url
-          origin_url
-          wallabag_created_at
-        }
-      }
-    }
-  `);
-  const failingUrls = [
-    'blogs.vmware.com',
-    'https://www.how2shout.com/linux/2-ways-to-install-cassandra-on-ubuntu-22-04-lts-jammy/',
-    'blog.softwaremill',
-    'www.ktexperts',
-    'rustyrazorblade.com',
-    'https://docs.d2iq.com/mesosphere/dcos/services/cassandra/2.9.0-3.11.6/security/',
-    '/www.an10.io/',
-    'itnext.io',
-    '/www.an10.io/',
-    'baeldung.com',
-    '/levelup.gitconnected',
-    'cassandra.apache.org/blog',
-    'Failed to fetch thumbnail for event: https://docs.d2iq.com/mesosphere/dcos/services/cassandra/2.9.0-3.11.6/security/',
-    'datanami.com',
-    'https://dataedo.com/',
-    'https://www.datastax.com/resources/webinar/serverless-functions-datastax-drivers',
-    'https://towardsdatascience.com/',
-    'https://medium.com/better-programming/our-favorite-engineering-blogs-3d8365b2d871',
-    'https://datastation.multiprocess',
-    'tobert.github.io',
-    'zeppelin.apache.org',
-    'https://docs.datastax.com/en/articles/cassandra/cassandrathenandnow.html',
-  ];
-
-  const filteredNodes = allLeavesPictures?.data?.allApiLeaves?.nodes.filter(
-    (node) => {
-      return !failingUrls.some((url) => node.url.includes(url));
-    }
-  );
-
-  //@ts-ignore
-  filteredNodes.forEach((node, index) => {
-    setTimeout(() => {
-      fetchThumbnail(node, createNode, createNodeId, getCache);
-    }, index * 200);
-  });
-
-  //----------------------------------------------------------------------------
-  //TTRS Pictures Processing
-  const AllNews: {
-    errors?: any;
-    data?: {
-      allFeedTtrs: {
-        nodes: {
-          id: string;
-          link: string;
-        }[];
-      };
-    };
-  } = await graphql(`
-    query NewsPictures {
-      allFeedTtrs {
-        nodes {
-          link
-          id
-        }
-      }
-    }
-  `);
-  const filterednewsNodes = AllNews?.data?.allFeedTtrs?.nodes.filter((node) => {
-    return !failingUrls.some((url) => node.link.includes(url));
-  });
-
-  //@ts-ignore
-  filterednewsNodes.forEach((node, index) => {
-    setTimeout(() => {
-      const newNode = {
-        id: node.id,
-        url: node.link,
-        origin_url: node.link,
-      };
-      fetchThumbnail(newNode, createNode, createNodeId, getCache);
-    }, index * 200);
   });
 
   //----------------------------------------------------------------------------
--- src/utils/fetch-thumbnail.tsx
+++ src/utils/fetch-thumbnail.tsx
@@ -12,58 +12,61 @@ const fetchThumbnail = async (
   createNodeId: any,
   getCache: any
 ) => {
-  let originalUrl = node.url
-  if (node.url.includes("youtube.com")) {
-    originalUrl = node.origin_url;
-  }
-  else {
-    originalUrl = node.url;
-  }
-
-  if (!originalUrl) {
-    console.error(`No original URL found for event: ${node.id}`);
-    return;
-  }
-
-  try {
-
-    const response = await fetch(
-      `https://iframe.ly/api/iframely?url=${originalUrl}&api_key=43dbc2d36e2b9a0b35ad8f&iframe=1&omit_script=1`
-    );
-    const data = await response.json();
-
-    let imageURL;
-    if (data.links.thumbnail) {
-      imageURL = data.links.thumbnail[0].href;
-    } else if (data.links.icon) {
-      imageURL = data.links.icon[0].href;
-    } else {
-      imageURL = "https://i.ibb.co/Bq2J6JT/Static-Thumbnail.png";
+  return new Promise(async (resolve, reject) => {
+    let originalUrl = node.url
+    if (node.url.includes("youtube.com")) {
+      originalUrl = node.origin_url;
+    }
+    else {
+      originalUrl = node.url;
     }
 
-    if (imageURL) {
-      const fileNode = await createRemoteFileNode({
-        url: imageURL,
-        parentNodeId: node.id,
-        createNode,
-        createNodeId,
-        getCache,
-      });
+    if (!originalUrl) {
+      console.error(`No original URL found for event: ${node.id}`);
+      return;
+    }
 
-      if (!fileNode) {
+    try {
+
+      const response = await fetch(
+        `https://iframe.ly/api/iframely?url=${originalUrl}&api_key=43dbc2d36e2b9a0b35ad8f&iframe=1&omit_script=1`
+      );
+      const data = await response.json();
+
+      let imageURL;
+      if (data.links.thumbnail) {
+        imageURL = data.links.thumbnail[0].href;
+      } else if (data.links.icon) {
+        imageURL = data.links.icon[0].href;
+      } else {
+        imageURL = "https://i.ibb.co/Bq2J6JT/Static-Thumbnail.png";
+      }
+
+      if (imageURL) {
+        const fileNode = await createRemoteFileNode({
+          url: imageURL,
+          parentNodeId: node.id,
+          createNode,
+          createNodeId,
+          getCache,
+        });
+
+        if (!fileNode) {
+          console.error(
+            `Failed to create file node for cover image: ${node.url} - image URL not found`
+          );
+        }
+      } else {
         console.error(
           `Failed to create file node for cover image: ${node.url} - image URL not found`
         );
       }
-    } else {
-      console.error(
-        `Failed to create file node for cover image: ${node.url} - image URL not found`
-      );
+    } catch (error) {
+      //@ts-ignore
+      console.error(`Failed to fetch thumbnail for event: ${node.url} with error: ${error.message}`);
     }
-  } catch (error) {
-    //@ts-ignore
-    console.error(`Failed to fetch thumbnail for event: ${node.url} with error: ${error.message}`);
-  }
+    resolve(true);
+  });
 };
 
 export default fetchThumbnail;
