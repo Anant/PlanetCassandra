1d947c4ff0c8afec5c6011f9599f4d02fe78c774
Andrej Hristovski <andrejhristovski@yahoo.com>
2023-04-18 11:43:21 +0200

    Fix url reddirect for use cases

--- src/components/Cards/UseCaseCard.tsx
+++ src/components/Cards/UseCaseCard.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import { Box, Typography, Button } from '@mui/material';
-import { IGatsbyImageData, StaticImage } from 'gatsby-plugin-image';
-import { CiFacebook, CiLinkedin, CiTwitter } from 'react-icons/ci';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import { Box, Typography, Button } from "@mui/material";
+import { IGatsbyImageData, StaticImage } from "gatsby-plugin-image";
+import { CiFacebook, CiLinkedin, CiTwitter } from "react-icons/ci";
 import { Link } from "gatsby";
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import getSlug from 'speakingurl';
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import getSlug from "speakingurl";
 
 interface Props {
   name: string;
@@ -17,26 +17,30 @@ interface Props {
 }
 
 const UseCaseCard = ({ name, description, url, gatsbyImageData }: Props) => {
+  const caseUrl = `/usecases/${getSlug(name)}`;
+
   return (
-    <Card sx={{ height: '480px', borderRadius: 5, margin: { xs: 'auto', md: 0 } }}>
+    <Card
+      sx={{ height: "480px", borderRadius: 5, margin: { xs: "auto", md: 0 } }}
+    >
       <Box sx={{ padding: 3 }}>
         <CardContent
           sx={{
             padding: 0,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            height: '100%',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "100%",
           }}
         >
           {gatsbyImageData && (
             <Box
               sx={{
-                height: '200px',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                overflow: 'hidden',
+                height: "200px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                overflow: "hidden",
                 borderRadius: 2,
               }}
             >
@@ -49,7 +53,7 @@ const UseCaseCard = ({ name, description, url, gatsbyImageData }: Props) => {
           )}
 
           <Typography
-            sx={{ color: '#5ab1bb', fontSize: 20, mt: 2 }}
+            sx={{ color: "#5ab1bb", fontSize: 20, mt: 2 }}
             gutterBottom
             variant="h6"
             className="Font_Montserrat_Bold truncate"
@@ -67,37 +71,51 @@ const UseCaseCard = ({ name, description, url, gatsbyImageData }: Props) => {
         </CardContent>
         <CardActions
           sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
+            display: "flex",
+            justifyContent: "space-between",
             paddingInline: 0,
-            alignItems: 'center',
+            alignItems: "center",
           }}
         >
-          <Link style={{ textDecoration: 'none' }} to={url}>
+          <Link style={{ textDecoration: "none" }} to={caseUrl}>
             <Button
               sx={{
                 borderRadius: 50,
-                backgroundColor: '#5AB1BB',
+                backgroundColor: "#5AB1BB",
                 fontSize: 10,
-                '&:hover': {
-                  backgroundColor: '#344D67',
+                "&:hover": {
+                  backgroundColor: "#344D67",
                 },
-                textTransform: 'none',
+                textTransform: "none",
               }}
               variant="contained"
             >
-              <Typography className="Font_Mulish_Button_M">Continue Reading</Typography>
+              <Typography className="Font_Mulish_Button_M">
+                Continue Reading
+              </Typography>
             </Button>
           </Link>
           <Box
             sx={{
-              display: 'flex',
-              alignItems: 'center',
+              display: "flex",
+              alignItems: "center",
             }}
           >
-            <CiFacebook style={{ cursor: 'pointer' }} color="#32A5EE" size="24px" />
-            <CiLinkedin style={{ cursor: 'pointer' }} color="#32A5EE" size="24px" />
-            <CiTwitter style={{ cursor: 'pointer' }} color="#32A5EE" size="24px" />
+            <CiFacebook
+              style={{ cursor: "pointer" }}
+              color="#32A5EE"
+              size="24px"
+            />
+            <CiLinkedin
+              style={{ cursor: "pointer" }}
+              color="#32A5EE"
+              size="24px"
+            />
+            <CiTwitter
+              style={{ cursor: "pointer" }}
+              color="#32A5EE"
+              size="24px"
+            />
           </Box>
         </CardActions>
       </Box>
