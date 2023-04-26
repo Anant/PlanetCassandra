8d233db51435bbc1cde1a21f890180dc010a107e
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-04-26 17:12:05 +0200

    Carousel styling updates

--- src/components/Hero/Carousel/newCarousel.tsx
+++ src/components/Hero/Carousel/newCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Typography,
   Paper,
@@ -6,13 +6,13 @@ import {
   Button,
   Card,
   CardContent,
-} from "@mui/material";
-import { Link } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image"
+  Box,
+} from '@mui/material';
+import { Link } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
 import { IGatsbyImageData } from 'gatsby-plugin-image';
-import Carousel from "react-material-ui-carousel";
-import SendIcon from "@mui/icons-material/Send";
-
+import Carousel from 'react-material-ui-carousel';
+import SendIcon from '@mui/icons-material/Send';
 
 interface Props {
   items: Array<{
@@ -34,62 +34,74 @@ const NewCarousel: React.FC<Props> = ({ items }) => {
   return (
     <Carousel onChange={(e) => handleNext(e)}>
       {items.map((item, i) => (
-        <Card key={i} sx={{
-          borderRadius: '16px',
-          boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)',
-          border: '3px solid #F2F2F2',
-        }}>
-          <CardContent sx={{ height: {
-            xs: "300px",
-            sm: "450px",
-            md: "400px",
-            lg: "400px"
-          } }}>
-          {item.image && isImageLoaded ? (
-            <GatsbyImage
-              image={items[currentIndex].image}
-              alt="Logo"
-              onError={() => setIsImageLoaded(false)}
-            />
-          ) : (
-            <Skeleton variant="rectangular" width={"100%"} height={300} />
-          )}
-          <Typography
-            marginTop={2}
-            textAlign={"center"}
-            height="80px"
-            variant="h5"
+        <Card
+          key={i}
+          sx={{
+            borderRadius: '16px',
+            boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)',
+            border: '3px solid #F2F2F2',
+          }}
+        >
+          <CardContent
             sx={{
-              paddingBottom: { xs: 2.5, sm: 1, md: 1, lg: 1 },
+              height: {
+                xs: '400px',
+                sm: '450px',
+                md: '400px',
+                lg: '489px',
+              },
             }}
           >
-            {item.title}
-          </Typography>
+            {item.image && isImageLoaded ? (
+              <Box sx={{ height: { xs: '60%', lg: '80%' } }}>
+                <GatsbyImage
+                  image={items[currentIndex].image}
+                  style={{ height: '100%' }}
+                  objectFit="contain"
+                  alt="Logo"
+                  onError={() => setIsImageLoaded(false)}
+                />
+              </Box>
+            ) : (
+              <Skeleton variant="rectangular" width={'100%'} height={300} />
+            )}
+            <Typography
+              marginTop={2}
+              textAlign={'center'}
+              height="80px"
+              variant="h5"
+              sx={{
+                paddingBottom: { xs: 2.5, sm: 1, md: 1, lg: 1 },
+              }}
+            >
+              {item.title}
+            </Typography>
           </CardContent>
 
-          <Link
-            style={{
-              display: "block",
-              textDecoration: "none",
-              color: "white",
-              margin: "auto",
-              textAlign: "center",
-            }}
-            to={`/post/${items[currentIndex].slug}`}
-          >
-            <Button
-              sx={{
-                borderRadius: 50,
-                marginBottom: 2,
-
-                backgroundColor: "#344D67",
+          <Box>
+            <Link
+              style={{
+                display: 'block',
+                textDecoration: 'none',
+                color: 'white',
+                margin: 'auto',
+                textAlign: 'center',
               }}
-              variant="contained"
-              endIcon={<SendIcon />}
+              to={`/post/${items[currentIndex].slug}`}
             >
-              Continue reading{" "}
-            </Button>
-          </Link>
+              <Button
+                sx={{
+                  borderRadius: 50,
+                  marginBottom: 2,
+                  backgroundColor: '#344D67',
+                }}
+                variant="contained"
+                endIcon={<SendIcon />}
+              >
+                Continue reading{' '}
+              </Button>
+            </Link>
+          </Box>
         </Card>
       ))}
     </Carousel>
