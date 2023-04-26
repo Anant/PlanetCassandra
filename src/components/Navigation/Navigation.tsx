24852834223390cc2e9d0588e7363da3ff7165e9
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-04-26 17:16:59 +0200

    Removed classes for font family, entered inline

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,25 +1,25 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-import SearchBar from "../SearchBar/SearchBar";
-import ToggleButton from "../ToggleButton/ToggleButton";
-import { StaticImage } from "gatsby-plugin-image";
-import { Link } from "gatsby";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import SearchBar from '../SearchBar/SearchBar';
+import ToggleButton from '../ToggleButton/ToggleButton';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
 const pages = [
-  { name: "Home", route: "/" },
-  { name: "Events", route: "https://blog.planetcassandra.org/events" },
-  { name: "News", route: "/news" },
-  { name: "Use Cases", route: "/usecases" },
-  { name: "Posts", route: "/posts" },
-  { name: "Resources", route: "/leaves" },
-  { name: "Videos", route: "/videos" },
+  { name: 'Home', route: '/' },
+  { name: 'Events', route: 'https://blog.planetcassandra.org/events' },
+  { name: 'News', route: '/news' },
+  { name: 'Use Cases', route: '/usecases' },
+  { name: 'Posts', route: '/posts' },
+  { name: 'Resources', route: '/leaves' },
+  { name: 'Videos', route: '/videos' },
 ];
 
 function ResponsiveAppBar() {
@@ -38,21 +38,21 @@ function ResponsiveAppBar() {
   return (
     <AppBar
       sx={{
-        bgcolor: "#F9F8F8",
-        boxShadow: "none",
+        bgcolor: '#F9F8F8',
+        boxShadow: 'none',
       }}
       position="static"
     >
       <Toolbar
         sx={{
-          display: "flex",
-          justifyContent: "space-between",
+          display: 'flex',
+          justifyContent: 'space-between',
           flexGrow: 1,
           marginX: 5,
         }}
         disableGutters
       >
-        <Box sx={{ display: { xs: "none", md: "block" } }}>
+        <Box sx={{ display: { xs: 'none', md: 'block' } }}>
           <Link to="/">
             <StaticImage
               src="../../images/LogoWithText.png"
@@ -67,7 +67,7 @@ function ResponsiveAppBar() {
         <Box
           sx={{
             flexGrow: 1,
-            display: { xs: "flex", md: "none" },
+            display: { xs: 'flex', md: 'none' },
           }}
         >
           <IconButton
@@ -83,23 +83,23 @@ function ResponsiveAppBar() {
             id="menu-appbar"
             anchorEl={anchorElNav}
             anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
+              vertical: 'bottom',
+              horizontal: 'left',
             }}
             keepMounted
             transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
+              vertical: 'top',
+              horizontal: 'left',
             }}
             open={Boolean(anchorElNav)}
             onClose={handleCloseNavMenu}
             sx={{
-              display: { xs: "block", md: "none" },
+              display: { xs: 'block', md: 'none' },
             }}
           >
             {pages.map(({ name, route }) => (
               <MenuItem key={name} onClick={handleCloseNavMenu}>
-                <Link key={name} to={route} style={{ textDecoration: "none" }}>
+                <Link key={name} to={route} style={{ textDecoration: 'none' }}>
                   <Typography textAlign="center">{name}</Typography>
                 </Link>
               </MenuItem>
@@ -109,19 +109,26 @@ function ResponsiveAppBar() {
 
         <Box
           sx={{
-            display: { xs: "none", md: "flex" },
+            display: { xs: 'none', md: 'flex' },
           }}
         >
           {pages.map((page) => (
             <Link
               key={page.name}
               to={page.route}
-              style={{ textDecoration: "none" }}
+              style={{ textDecoration: 'none' }}
             >
               <Button
                 onClick={handleCloseNavMenu}
-                className="Font_Poppins_Regular"
-                sx={{ my: 2, color: "black", fontSize: 12, display: "block" }}
+                sx={{
+                  my: 2,
+                  fontFamily: 'Poppins, sans-serif',
+                  fontWeight: 400,
+                  color: 'black',
+                  fontSize: '12px',
+                  display: 'block',
+                  textTransform: 'capitalize',
+                }}
               >
                 {page.name}
               </Button>
@@ -131,21 +138,33 @@ function ResponsiveAppBar() {
         <SearchBar />
         <Box
           sx={{
-            display: { xs: "none", md: "flex" },
+            display: { xs: 'none', md: 'flex' },
           }}
         >
-          <Link style={{ textDecoration: "none" }} to={`/contribute`}>
+          <Link style={{ textDecoration: 'none' }} to={`/contribute`}>
             <Button
-              sx={{ my: 2, color: "black", fontSize: 12 }}
-              className="Font_Poppins_Regular"
+              sx={{
+                my: 2,
+                color: 'black',
+                fontSize: '12px',
+                fontFamily: 'Poppins, sans-serif',
+                fontWeight: 400,
+                textTransform: 'capitalize',
+              }}
             >
               Contribute
             </Button>
           </Link>
-          <Link style={{ textDecoration: "none" }} to={`/contact`}>
+          <Link style={{ textDecoration: 'none' }} to={`/contact`}>
             <Button
-              className="Font_Poppins_Regular"
-              sx={{ my: 2, color: "black", fontSize: 12 }}
+              sx={{
+                my: 2,
+                color: 'black',
+                fontSize: '12px',
+                fontFamily: 'Poppins, sans-serif',
+                fontWeight: 400,
+                textTransform: 'capitalize',
+              }}
             >
               Contact
             </Button>
