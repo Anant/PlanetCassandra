2a961b7331bf01d2377d04c81d0c055730fc8df9
Andrej Hristovski <andrejhristovski@yahoo.com>
2023-05-12 16:12:45 +0200

    Declare module for react helmet

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,6 +2,7 @@ declare module "gatsby-plugin-mailchimp";
 declare module "*.png";
 declare module "showdown";
 declare module "react-jotform-embed";
+declare module "react-helmet";
 declare namespace NodeJS {
   export interface ProcessEnv {
     ALGOLIA_APP_ID: string;
