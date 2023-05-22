6400b23092836ba002a0155e3d673e573a700c5e
Andrej Hristovski <andrejhristovski@yahoo.com>
2023-05-22 15:02:22 +0200

    Fix carousel bug

--- src/components/Hero/Carousel/newCarousel.tsx
+++ src/components/Hero/Carousel/newCarousel.tsx
@@ -1,18 +1,10 @@
 import React, { useState } from "react";
-import {
-  Typography,
-  Paper,
-  Skeleton,
-  Button,
-  Card,
-  CardContent,
-} from "@mui/material";
+import { Typography, Skeleton, Button, Card, CardContent } from "@mui/material";
 import { Link } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image"
-import { IGatsbyImageData } from 'gatsby-plugin-image';
+import { GatsbyImage } from "gatsby-plugin-image";
+import { IGatsbyImageData } from "gatsby-plugin-image";
 import Carousel from "react-material-ui-carousel";
 import SendIcon from "@mui/icons-material/Send";
-
 
 interface Props {
   items: Array<{
@@ -34,37 +26,42 @@ const NewCarousel: React.FC<Props> = ({ items }) => {
   return (
     <Carousel onChange={(e) => handleNext(e)}>
       {items.map((item, i) => (
-        <Card key={i} sx={{
-          borderRadius: '16px',
-          boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)',
-          border: '3px solid #F2F2F2',
-        }}>
-          <CardContent sx={{ height: {
-            xs: "300px",
-            sm: "450px",
-            md: "400px",
-            lg: "450px"
-          } }}>
-          {item.image && isImageLoaded ? (
-            <GatsbyImage
-              image={items[currentIndex].image}
-              alt="Logo"
-              onError={() => setIsImageLoaded(false)}
-            />
-          ) : (
-            <Skeleton variant="rectangular" width={"100%"} height={300} />
-          )}
-          <Typography
-            marginTop={2}
-            textAlign={"center"}
-            height="80px"
-            variant="h5"
+        <Card
+          key={i}
+          sx={{
+            borderRadius: "16px",
+            boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.1)",
+            border: "3px solid #F2F2F2",
+          }}
+        >
+          <CardContent
             sx={{
-              paddingBottom: { xs: 2.5, sm: 1, md: 1, lg: 1 },
+              height: {
+                xs: "300px",
+                sm: "450px",
+                md: "400px",
+                lg: "450px",
+              },
             }}
           >
-            {item.title}
-          </Typography>
+            {item.image && isImageLoaded ? (
+              <GatsbyImage
+                style={{ height: "80%" }}
+                image={items[currentIndex].image}
+                alt="Logo"
+                onError={() => setIsImageLoaded(false)}
+              />
+            ) : (
+              <Skeleton variant="rectangular" width={"100%"} height={380} />
+            )}
+            <Typography
+              className="textTruncate-2"
+              marginTop={2}
+              textAlign={"center"}
+              variant="h5"
+            >
+              {item.title}
+            </Typography>
           </CardContent>
 
           <Link
