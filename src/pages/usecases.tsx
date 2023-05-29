a86ddc184b32b0b9536a3c4ba8ff54724094de91
Andrej Hristovski <andrejhristovski@yahoo.com>
2023-05-29 16:42:34 +0200

    Implement algolia facets to use cases

--- src/pages/usecases.tsx
+++ src/pages/usecases.tsx
@@ -1,65 +1,28 @@
-import React, { useState } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Layout from '../components/Layout/Layout';
-import { Container, Grid, Pagination } from '@mui/material';
-import CompanyCard from '../components/Cards/CompanyCard';
-import UseCaseGrid from '../layouts/UseCaseGrid';
-import { IGatsbyImageData } from 'gatsby-plugin-image';
+import React, { useState } from "react";
+import Layout from "../components/Layout/Layout";
+
 //@ts-ignore
-import { Helmet } from 'react-helmet';
-interface CompanyData {
-  allAirtable: {
-    nodes: {
-      table: string;
-      data: {
-        Case_Name: string;
-        Case_Description: string;
-        Case_URL: string;
-        ID_Case: number;
-        Case_Article_Content: string;
-        Case_Company: {
-          data: {
-            Name: string;
-          };
-        }[];
-        gatsbyImageData: IGatsbyImageData | null;
-      };
-      downloadedImages: {
-        id: string;
-        childImageSharp: {
-          gatsbyImageData: IGatsbyImageData;
-        }
-      }[];
-    }[];
-  };
-}
+import { Helmet } from "react-helmet";
+import CategoryIndex from "../layouts/SearchLayout/CategoryIndex";
+import { InstantSearch } from "react-instantsearch-hooks-web";
+import algoliasearch from "algoliasearch";
+import CustomSearchBox from "../layouts/SearchLayout/CustomSearchBox";
+import { Container } from "@mui/material";
 
-interface LogoFile {
-  name: string;
-  childrenImageSharp: {
-    gatsbyImageData: IGatsbyImageData;
-  }[];
-}
-
-const Companies: React.FC<CompanyData> = () => {
-  const data = useStaticQuery(query);
-  const allAirtable = data.allAirtable as CompanyData['allAirtable'];
-  const allFile = data.allFile.nodes as LogoFile[];
-
-  const currentCompanies = allAirtable.nodes.map((node) => {
-    return {
-      ...node.data,
-      gatsbyImageData: node?.downloadedImages[0]?.childImageSharp?.gatsbyImageData || null,
-    };
-  });
+const Companies: React.FC<any> = () => {
+  const [query, setQuery] = useState("");
+  const searchClient = algoliasearch(
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_API_KEY
+  );
   return (
     <Layout>
       <Helmet>
         <title>Use Cases - Planet Cassandra</title>
         <meta property="og:image" content="../images/icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content={'Use Cases - Planet Cassandra'} />
-        <meta name="author" content={'Planet Cassandra'} />
+        <meta property="og:title" content={"Use Cases - Planet Cassandra"} />
+        <meta name="author" content={"Planet Cassandra"} />
         <meta
           name="keywords"
           content="Cassandra use cases, data-driven solutions, IoT, e-commerce, analytics"
@@ -73,48 +36,35 @@ const Companies: React.FC<CompanyData> = () => {
           content="Explore various use cases for Cassandra database on Planet Cassandra. Learn how Cassandra is being used in real-world applications for data-driven solutions, including IoT, e-commerce, analytics, and more."
         />
       </Helmet>
-      <UseCaseGrid cardData={currentCompanies}/>
+      <Container maxWidth="xl" sx={{ marginY: 3 }}>
+        <InstantSearch
+          indexName="PlanetCassandraUseCases"
+          searchClient={searchClient}
+          insights
+        >
+          <CustomSearchBox defaultQuery={query} setDefaultQuery={setQuery} />
+          <CategoryIndex
+            category={"usecases"}
+            cardType={"usecases"}
+            indexName={"PlanetCassandraUseCases"}
+            title={"Use Cases"}
+            hideTitle={true}
+            refinementLists={[
+              {
+                attribute: "data.Case_Function.data.Function_Name",
+                label: "Function",
+              },
+              { attribute: "data.Case_Stack.data.Name", label: "Stack" },
+              {
+                attribute: "data.Case_Industry.data.Industry_Name",
+                label: "Industry",
+              },
+            ]}
+          />
+        </InstantSearch>
+      </Container>
     </Layout>
   );
 };
 
-const query = graphql`
-  query UseCasesDataListing {
-    allAirtable(
-      filter: {table: {eq: "Cases"}, data: {Case_URL: {ne: null}}}
-      sort: {data: {Case_Published: DESC}}
-    ) {
-      nodes {
-        table
-        data {
-          Case_URL
-          Case_Name
-          ID_Case
-          Case_Article_Content
-          Case_Description
-          Case_Company {
-            data {
-              Name
-            }
-          }
-        }
-        downloadedImages {
-          id
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
-      }
-    }
-    allFile(filter: { id: { ne: null } }) {
-      nodes {
-        name
-        childrenImageSharp {
-          gatsbyImageData
-        }
-      }
-      totalCount
-    }
-  }
-`;
 export default Companies;
