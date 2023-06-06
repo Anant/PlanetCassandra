f115578c311658fb1ef3cfb001560f37569c1e1c
Andrej Hristovski <andrejhristovski@yahoo.com>
2023-06-06 16:57:16 +0200

    Redesign 404 page and change forms in contribute page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -22,39 +22,84 @@ const NotFoundPage = () => (
             <Box>
               <Typography
                 sx={{
+                  color: "#344D67",
                   textTransform: "capitalize",
-                  fontWeight: 700,
-                  fontSize: { xs: "50px", md: "60px", lg: "83px" },
+                  fontWeight: 500,
+                  fontSize: { xs: "40px", md: "60px", lg: "60px" },
                 }}
-                fontFamily={"Poppins,sans-serif"}
+                fontFamily={"Montserrat,sans-serif"}
               >
-                404
-              </Typography>
-              <Typography
-                sx={{
-                  textTransform: "capitalize",
-                  fontWeight: 700,
-                  fontSize: { xs: "50px", md: "60px", lg: "83px" },
-                }}
-                fontFamily={"Poppins,sans-serif"}
-              >
-                error page{" "}
+                Something's missing?
               </Typography>
             </Box>
             <Box
               sx={{ marginY: { xs: "35px", md: "55px" }, width: { sm: "70%" } }}
             >
               <Typography
-                sx={{ fontSize: { xs: "20px", sm: "18px", lg: "24px" } }}
-                fontFamily={"Poppins, sans-serif"}
+                sx={{
+                  color: "#1E1E1E",
+                  fontSize: { xs: "20px", sm: "18px", lg: "24px" },
+                  fontWeight: 300,
+                }}
+                fontFamily={"Lato, sans-serif"}
               >
-                Oops, the page you're looking for can't be found!
+                Help us fill in the gaps!{" "}
+              </Typography>
+              <Typography
+                sx={{
+                  color: "#1E1E1E",
+                  fontWeight: 300,
+                  marginTop: 1,
+                  fontSize: { xs: "20px", sm: "18px", lg: "24px" },
+                }}
+                fontFamily={"Lato, sans-serif"}
+              >
+                Your contribution might be just what we need.
+              </Typography>
+              <Typography
+                sx={{
+                  color: "#344D67",
+                  fontWeight: 600,
+                  marginTop: 3,
+                  fontSize: { xs: "20px", sm: "18px", lg: "32px" },
+                }}
+                fontFamily={"Lato, sans-serif"}
+              >
+                We build together!
               </Typography>
             </Box>
             <Box>
+              <Link to={"/contribute"}>
+                <Button
+                  sx={{
+                    width: { xs: "250px", lg: "350px", xl: "400px" },
+                    textTransform: "capitalize",
+                    borderRadius: "15px",
+                    padding: { xs: "11px 30px", lg: "11px 55px" },
+                    background: "#283B4F",
+                    "&:hover": {
+                      background: "#283B4F",
+                      color: "#FFFFFF",
+                    },
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      color: "#FFFFFF",
+                      fontSize: { sm: "18px", xl: "27px" },
+                    }}
+                    fontFamily={"Open Sans,sans-serif"}
+                  >
+                    Become a contributor
+                  </Typography>
+                </Button>
+              </Link>
+            </Box>
+            <Box sx={{ marginTop: 3 }}>
               <Link to={"/"}>
                 <Button
                   sx={{
+                    width: { xs: "250px", lg: "350px", xl: "400px" },
                     textTransform: "capitalize",
                     borderRadius: "15px",
                     padding: { xs: "11px 30px", lg: "11px 55px" },
@@ -68,7 +113,7 @@ const NotFoundPage = () => (
                   <Typography
                     sx={{
                       color: "#FFFFFF",
-                      fontSize: { sm: "18px", xl: "28px" },
+                      fontSize: { sm: "18px", xl: "27px" },
                     }}
                     fontFamily={"Open Sans,sans-serif"}
                   >
--- src/pages/contribute.tsx
+++ src/pages/contribute.tsx
@@ -53,6 +53,7 @@ const ContributePage = () => {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: { xs: "70%", md: "50%" },
+    height: "80%",
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
@@ -116,7 +117,7 @@ const ContributePage = () => {
           <iframe
             src={contributingListData[modalLink].link}
             width="100%"
-            height="533"
+            height="100%"
           ></iframe>
         </Box>
       </Modal>
