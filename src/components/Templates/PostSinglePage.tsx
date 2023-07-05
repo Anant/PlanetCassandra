ff04443f2da1ed189ba582fb93cae387a666b76c
Andrej Hristovski <andrejhristovski@yahoo.com>
2023-07-05 16:28:19 +0200

    Fix gatsby image on posts single page

--- src/components/Templates/PostSinglePage.tsx
+++ src/components/Templates/PostSinglePage.tsx
@@ -106,8 +106,11 @@ const PostSinglePage: React.FC<PostSinglePageProps> = ({
               className="thumbnail-img-box img"
               sx={{ width: "100%", marginBottom: "30px", textAlign: "center" }}
             >
-              <img src={metaImage} />
-              {/* <GatsbyImage image={featuredImage} alt={title} /> */}
+              <GatsbyImage
+                image={featuredImage}
+                alt={title}
+                imgStyle={{ objectFit: "contain" }}
+              />
             </Box>
             <Typography
               gutterBottom
